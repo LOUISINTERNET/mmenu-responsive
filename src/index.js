34715ff425eff526cbca4309d8421d638a792159
Patrick Kahl <Kahl.Patrick@googlemail.com>
2018-12-18 14:43:11 +0100

    Opt code

--- src/index.js
+++ src/index.js
@@ -3,18 +3,62 @@ const responsiveMmenu = (
   navSelector = '#nav',
   breakpoint = 1279
 ) => {
+
+  const parent = document.querySelector(navSelector).parentNode
+  let nav
+  let mmenu
+
+  /**
+   * Return current browser width
+   * @ return float
+   */
   const getBrowserWidth = () =>
     Math.max(document.documentElement.clientWidth, window.innerWidth || 0)
-  const cloneNav = () =>
-    (nav = document.querySelector(navSelector).cloneNode(true))
-  const parent = document.querySelector(navSelector).parentNode
-  const switchFromMobilToDesktop = () => {
+
+  /**
+   * Return if the desktop nav could be init
+   * @return boolean
+   */
+  const isDesktop = () => getBrowserWidth() > breakpoint && mmenu !== undefined
+
+  /**
+   * Add the desktop nav and trigger remove mmenu
+   */
+  const addDesktop = () => {
+    if (!isMmenuOpen()) {
+      removeMmenu()
+      parent.appendChild(nav)
+    }
+  }
+
+  /**
+   * Return if the mmenu nav could be init
+   * @return boolean
+   */
+  const isMmenu = () => getBrowserWidth() <= breakpoint && !mmenu
+
+  /**
+   * Return is mmenu is open
+   * @return boolean
+   */
+  const isMmenuOpen = () => mmenu ? mmenu.getInstance().vars.opened : false
+
+  /**
+   * Remove mmenu and changes from mmenu
+   */
+  const removeMmenu = () => {
+    mmenu = undefined
+
     document.querySelector(navSelector).remove()
 
     const wrapper = document.querySelector('.mm-wrapper__blocker')
     if(wrapper) wrapper.remove()
 
-    document.querySelector('.mm-page').removeAttribute('style')
+    const blocker = document.querySelector('#mm-blocker')
+    if(blocker) blocker.remove()
+
+    const page = document.querySelector('.mm-page')
+    if(page) page.removeAttribute('style')
 
     const mmenuItemsWithId = document.querySelectorAll('[id*="mm-"]')
     mmenuItemsWithId.forEach(item => item.removeAttribute('id'))
@@ -23,35 +67,26 @@ const responsiveMmenu = (
     Array.from(mmenuItemsWithClass).forEach(item => {
       item.classList.remove.apply(item.classList, Array.from(item.classList).filter(v=>v.startsWith('mm-')));
     })
-    parent.appendChild(nav)
   }
 
-  let nav
-  let mmenu = null
+  /**
+   * Add mmenu
+   */
+  const addMmenu = () => {
+    createMmenu.call()
+    mmenu = $(navSelector).data('mmenu')
+    mmenu.bind('close', addDesktop)
+    mmenu.bind('close:finish', addDesktop)
+  }
 
   window.addEventListener('resize', () => {
-    if (getBrowserWidth() <= breakpoint && !mmenu) {
-      cloneNav()
-      createMmenu.call()
-      mmenu = $(navSelector).data('mmenu')
-    } else if (getBrowserWidth() > breakpoint && mmenu !== null) {
-
-      // For older mmenu versions
-      mmenu.bind('close', function () {
-        switchFromMobilToDesktop()
-      });
-
-      // For newer mmenu versions
-      mmenu.bind('close:finish', function () {
-        switchFromMobilToDesktop()
-      });
-
-      if(mmenu.getInstance().vars.opened) {
-        mmenu.close();
-      } else {
-        switchFromMobilToDesktop()
-      }
-      mmenu = null
+    if (isMmenu()) {
+      nav = document.querySelector(navSelector).cloneNode(true)
+      addMmenu()
+    } else if (isDesktop() && isMmenuOpen()) {
+      mmenu.close()
+    } else if (isDesktop()) {
+      addDesktop()
     }
   })
   window.dispatchEvent(new Event('resize'))
